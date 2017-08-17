eedbdaa1e65273bce060623a2c15f2b6928655aa
任波 <borenfocus@gmail.com>
2017-08-17 10:27:58 +0800

    初始化项目

---
+++ NetWorkManagerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWorkManagerTest
+//
+//  Created by 任波 on 2017/8/17.
+//  Copyright © 2017年 renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
